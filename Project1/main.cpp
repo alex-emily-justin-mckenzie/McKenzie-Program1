c8d2859fe58bc1f0326f301049ede68f98c3b7e0
McKenzie Humann <mhv3p@mail.umkc.edu>
2019-08-25 10:49:52 -0500

    Initial Commit

---
+++ Project1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project1
+//
+//  Created by McKenzie Humann on 8/25/19.
+//  Copyright © 2019 McKenzie Humann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
